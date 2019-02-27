920c71c3d3d0945fd26c7ae7a02708b92eb0a890
Zhenya <demichka@gmail.com>
2019-02-27 12:36:58 +0100

    modified style, NavBar murkup

--- src/NavBar/index.js
+++ src/NavBar/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { withRouter, Link} from "react-router-dom";
 import {
-  Nav
+  Navbar
 } from 'reactstrap';
 
 
 class NavBar extends React.Component {
   render () {
-    return <Nav className="nav flex-column flex-sm-row">
+    return <Navbar className="nav flex-column flex-sm-row">
               <Link className="flex-sm-fill flex-grow-1 text-sm-left nav-link active" to="/"><h1>Lumia</h1></Link>
               <Link className="text-sm-right nav-link" to="/contact-page">Kontakta oss</Link>
-            </Nav>
+            </Navbar>
       
   }
 }
